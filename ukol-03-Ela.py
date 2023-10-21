21c48bb57ca0330ad5b11d09e5d51f7fe8921761
Moje Jmeno <ela.ostapiuk@gmail.com>
2023-10-21 13:53:40 +0200

    Ukol 03

--- ukol-03-Ela.py
+++ ukol-03-Ela.py
@@ -3,8 +3,14 @@ import json
 with open ('body.json', mode='r', encoding='utf-8') as file:
     data = json.load(file)
 
+prospech = {}
 for jmeno, body in data.items():
     if body >= 60:
-        print(jmeno,":", "Pass")
+        prospech[jmeno] = "Pass"
     else:
-         print(jmeno,":", "Fail")
+        prospech[jmeno] = "Fail"
+
+print(prospech)
+
+with open("prospech.json", mode="w", encoding="utf-8") as file2:
+    json.dump(prospech, file2, ensure_ascii=False, indent=4)
